0d2f46ed386e5e5a8bae299f5f384b98f4aabed4
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-05-05 12:29:12 -0700

    Updated

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,7 @@
 window.addEventListener('load', function () {
-  // task 3
-  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
-
   // task 2
   const amenityIds = {};
-  $('input[type=checkbox]').click(function () {
+  $('input[type=checkbox]').change(function () {
     if ($(this).prop('checked')) {
       amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
     } else if (!$(this).prop('checked')) {
